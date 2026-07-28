b48ae9a80305c55b8a92b139237ee4d3e2d1569f
Camilo Vargas <camilo.vargas0419@hotmail.com>
2026-07-28 00:29:48 -0500

    fix: habilita panel administrativo de Django

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,7 @@
+from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("", include("inventarios.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("inventarios.urls")),
+]
